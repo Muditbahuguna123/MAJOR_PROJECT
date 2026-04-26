23a5bae206005599e77c010b3d33e33af6d8d1c2
ayush-raw14 <ayushrawat.aug@gmail.com>
2026-04-26 21:51:44 +1200

    frontend updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,23 @@
-import { useState } from "react";
-import Dashboard from "./pages/Dashboard";
-import CropRecommendation from "./pages/CropRecommendation";
-import DiseaseDetection from "./pages/DiseaseDetection";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Landing";
+import Dashboard from "./pages/Dashboard/index.jsx";
+import AnalyticsPage from "./pages/analytics/index.jsx";
+import Detection from "./pages/detection/index.jsx";
+import Logs from "./pages/system-logs";
+import Settings from "./pages/settings";
 
-function App() {
-  const [page, setPage] = useState("dashboard");
-
+export default function App() {
   return (
-    <div>
-      <div style={{ padding: "10px", background: "#eee" }}>
-        <button onClick={() => setPage("dashboard")}>
-          Dashboard
-        </button>
-        <button onClick={() => setPage("crop")}>
-          Crop Recommendation
-        </button>
-        <button onClick={() => setPage("disease")}>
-          Disease Detection
-        </button>
-      </div>
-
-      {page === "dashboard" && <Dashboard />}
-      {page === "crop" && <CropRecommendation />}
-      {page === "disease" && <DiseaseDetection />}
-    </div>
-  );
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Dashboard />} />
+              <Route path="analytics" element={<AnalyticsPage />} />
+              <Route path="detection" element={<Detection />} />
+              <Route path="system-logs" element={<Logs />} />
+              <Route path="settings" element={<Settings />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+  )
 }
-
-export default App;
